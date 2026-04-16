d6dbd5e95983b8d6d5c12b1bc43ef44cdb952f6b
Hillol Das Pias <hillol4958@gmail.com>
2026-04-17 02:51:18 +0600

    new project added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,6 +7,28 @@ import { ExternalLink, Github } from "lucide-react";
 const projects = [
   {
     id: 1,
+    title: "CookMind - AI Recipe Generator",
+    description:
+      "a Full Stack AI Recipe Platform SaaS App built with Next.Js 16 with Tailwind CSS, Strapi as Headless CMS, PostgreSQL-NeonDB, Google Authentication, Arcjet, Shadcn UI",
+    image: "/CookMind-screenshot.png",
+    tags: [
+      "Next.js",
+      "Strapi",
+      "Node.js",
+      "Express.js",
+      "PostgreSQL",
+      "NeonDB",
+      "Google Authentication",
+      "Arcjet",
+      "Tailwind CSS",
+      "Shadcn UI",
+    ],
+    category: "web",
+    github: "https://github.com/HillolPias/cookmind",
+    demo: "https://cookmind-ivory.vercel.app/",
+  },
+  {
+    id: 2,
     title: "Full-stack Chat App",
     description:
       "A real-time chat application with user authentication, real-time messaging, and image uploads. Built with Node.js, Express, MongoDB Atlas, Socket.io, and a React/Chakra UI frontend.",
@@ -25,7 +47,7 @@ const projects = [
   },
 
   {
-    id: 2,
+    id: 3,
     title: "Product Store App",
     description:
       "A simple and modern full-stack product management application where users can add, view, update, and delete products. Built with Node.js, Express, MongoDB Atlas, and a React/Chakra UI frontend.",
@@ -36,7 +58,7 @@ const projects = [
     demo: "https://product-store-app-eight.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     title: "SwaPE IT Solutions",
     description:
       "A modern, responsive business website built with Next.js and TypeScript, styled using Tailwind CSS and ShadCN, with smooth animations powered by Framer Motion.",
@@ -47,7 +69,7 @@ const projects = [
     demo: "https://www.swapeit.co.uk/",
   },
   {
-    id: 4,
+    id: 5,
     title: "Portfolio Landing Page",
     description:
       "A sleek and responsive portfolio built with Next.js, styled using Tailwind CSS, and enhanced with smooth animations via Framer Motion.",
@@ -58,7 +80,7 @@ const projects = [
     demo: "https://my-portfolio-five-theta-17.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     title: "NextLevel Food",
     description:
       "A fullstack community-driven web application for food sharing, built with Next.js and Supabase. Features include user authentication, image uploads, and dynamic content management.",
@@ -69,7 +91,7 @@ const projects = [
     demo: "https://next-level-food-blond.vercel.app/",
   },
   {
-    id: 6,
+    id: 7,
     title: "TechGear E-commerce App",
     description:
       "A sleek full-stack e-commerce platform built with Next.js, MongoDB, Stripe, and NextAuth, featuring modern UI with Tailwind CSS, ShadCN, Framer Motion, and form validation via Zod.",
@@ -89,7 +111,7 @@ const projects = [
     demo: "https://tech-gear-app.vercel.app/",
   },
   {
-    id: 7,
+    id: 8,
     title: "FitTrack - Health & Fitness App",
     description:
       "A web application for tracking workouts, nutrition, and health metrics with personalized recommendations.",
